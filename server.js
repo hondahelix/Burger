a0ed3607ba42aaae69cb8c0fde1452abe899dbeb
Jonathan Honda <hondahelix120@gmail.com>
2020-10-22 17:29:38 -0700

    added handlebars files

--- server.js
+++ server.js
@@ -2,3 +2,20 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 
 var app = express();
+ 
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home');
+});
+ 
+
+// var routes = require("./controllers/catsController.js");
+
+// app.use(routes);
+
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
