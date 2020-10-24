d2bdb1a746a366d4273cd52335ae7f54b19feed6
Jonathan Honda <hondahelix120@gmail.com>
2020-10-23 20:46:28 -0700

    finsihed code

--- config/orm.js
+++ config/orm.js
@@ -12,19 +12,18 @@ function printQuestionMarks(num) {
 
 function objToSql(ob) {
     var arr = [];
-  
     // loop through the keys and push the key/value as a string int arr
     for (var key in ob) {
       var value = ob[key];
       // check to skip hidden properties
       if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
         if (typeof value === "string" && value.indexOf(" ") >= 0) {
           value = "'" + value + "'";
         }
         arr.push(key + "=" + value);
       }
     }
+    // devour = '1'
     return arr;
 };
 var orm = {
@@ -39,7 +38,6 @@ var orm = {
     },
     insertOne: function(table, cols, vals, cb) {
       var queryString = "INSERT INTO " + table;
-  
       queryString += " (";
       queryString += cols.toString();
       queryString += ") ";
@@ -47,13 +45,12 @@ var orm = {
       queryString += printQuestionMarks(vals.length);
       queryString += ") ";
   
-      console.log(queryString);
+      //console.log(queryString);
   
       connection.query(queryString, vals, function(err, result) {
         if (err) {
           throw err;
         }
-  
         cb(result);
       });
     },
