370d27a694296fb4b663e56b0ee76812b88d250a
Jonathan Honda <hondahelix120@gmail.com>
2020-10-23 18:24:10 -0700

    set up page and ajax calls

--- server.js
+++ server.js
@@ -1,19 +1,21 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-
+var PORT = process.env.PORT || 8080;
 var app = express();
  
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set('view engine', 'handlebars');
  
-app.get('/', function (req, res) {
-    res.render('home');
-});
- 
 
-// var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controller.js");
 
-// app.use(routes);
+app.use(routes);
 
 app.listen(PORT, function() {
     // Log (server-side) when our server has started
